e840452639c6e40e019e8d79dd9c5f5c08d5e844
Ngeeslin <edward.geeslin@gmail.com>
2018-06-09 13:32:58 -0400

    added routes for hw2
    added routes for hw2

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/hwdata');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var hw1Router = require('./routes/hw1');
+var hw2Router = require('./routes/HW2');
 
 var app = express();
 
@@ -20,9 +23,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/hw1', hw1Router)
+app.use('/hw1', hw1Router);
+app.use('/HW2', hw2Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
